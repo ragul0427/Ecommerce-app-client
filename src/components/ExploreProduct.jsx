53fbdfb3f0cb37f0b07c0ed6b136cb802f6995f3
ragul0427 <ragulhp2704@gmail.com>
2023-10-25 22:43:53 +0530

    magni

--- src/components/ExploreProduct.jsx
+++ src/components/ExploreProduct.jsx
@@ -1,46 +1,96 @@
 import React, { useEffect, useState } from "react";
 import { Images } from "../helper/ExploreProducts";
 import { useLocation } from "react-router-dom";
+import ReactImageMagnify from "react-image-magnify";
 
 function ExploreProduct() {
   const [img, setImg] = useState(Images[0].img[0]);
-  const [products,setProducts] = useState("")
-  const location=useLocation()
+  const [products, setProducts] = useState("");
+  const location = useLocation();
 
-  console.log(location.pathname.split("/")[2])
+  console.log(location.pathname.split("/")[2]);
 
- useEffect(()=>{
- setProducts(Images.filter((res,i)=>{
-  return res.subCatId==location.pathname.split("/")[2]
-}))
- },[])
+  useEffect(() => {
+    setProducts(
+      Images.filter((res, i) => {
+        return res.subCatId == location.pathname.split("/")[2];
+      })
+    );
+  }, []);
 
- console.log(products,"Wesgy")
+  console.log(products, "Wesgy");
 
   return (
-    <div className="flex flex-col md:flex-row md:w-[85vw] justify-between m-auto mt-4">
-      <div className="md:w-[38vw] bg-white">
-        <div className="w-full  h-[60vh] flex items-center justify-center">
-          <img src={img} className="h-[80%]"/>
-        </div>
-        <div className="flex">
-          {products[0]?.img?.map((res, i) => {
-            return (
-                 <div
-                className={`w-[20vw] md:w-[10vw] bg-white hover:opacity-50 cursor-pointer ${res===img?"border-red-600 border-[4px]":"border-slate-300 border-[2px]"}`}
-                key={i}
-                onMouseOver={() => {
-                  setImg(res);
-                }}
-              >
-                <img src={res} />
-              </div>
+    <div className="bg-white md:!h-[100vh]">
+      <div className="flex flex-col md:flex-row md:w-[85vw] justify-between m-auto md:pt-5">
+        <div className="md:w-[40vw] bg-white border flex flex-row-reverse border-slate-300">
+          <div className="w-full  md:max-w-[100%] md:h-auto flex items-center justify-center">
+            <img src={img} className="w-[100%] h-auto md:hidden" />
+            <ReactImageMagnify
+            className="!hidden md:!flex"
+              {...{
+                smallImage: {
+                  alt: "Wristwatch by Ted Baker London",
+                  isFluidWidth: true,
+                  src: img, 
+                  
+                },
+                largeImage: {
+                  src: img,
+                  width: 1000,
+                  height: 1000,
+                },
+              }}
+              
+            />
+          </div>
+          <div className="flex flex-col">
+            {products[0]?.img?.map((res, i) => {
+              return (
+                <div
+                  className={`w-[20vw] md:w-[6.2vw] bg-white hover:opacity-50 cursor-pointer ${
+                    res === img
+                      ? "border-red-600 border-[4px]"
+                      : "border-slate-300 border-[2px]"
+                  }`}
+                  key={i}
+                  onMouseOver={() => {
+                    setImg(res);
+                  }}
+                >
+                  <img src={res} />
+                </div>
               );
-          })}
+            })}
+          </div>
         </div>
-      </div>
-      <div className="w-[38vw]">
-      <h1 className="text-3xl">{products[0]?.title}</h1>
+        <div className="w-[90vw] m-auto md:w-[38vw] flex flex-col">
+          <h1 className="pt-4 text-2xl md:text-4xl">{products[0]?.title}</h1>
+          <p classname="flex ">
+            <span>0 reviews</span>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <span>0 ratings</span>
+          </p>
+          {products[0]?.description.map((res, i) => {
+            return (
+              <ul key={i} className="flex flex-col gap-4 pt-3 pl-4">
+                <li className="list-disc">{res}</li>
+              </ul>
+            );
+          })}
+
+          <div>
+            <p className="pt-5">Choose Color</p>
+            <div className="flex gap-1 pt-1">
+              <div className="!w-[22vw] md:!w-[6vw] border-2 border-slate-300 hover:scale-105 duration-1000 cursor-pointer">
+                <img src="https://tiimg.tistatic.com/fp/1/007/951/6-6-inch-screen-size-full-hd-128-gb-internal-storage-vivo-y33t-mobile-phone-447.jpg" />
+              </div>
+              <div className="!w-[22vw] md:!w-[6vw] border-2 border-slate-300 hover:scale-105 duration-1000 cursor-pointer">
+                <img src="https://tiimg.tistatic.com/fp/1/007/951/6-6-inch-screen-size-full-hd-128-gb-internal-storage-vivo-y33t-mobile-phone-447.jpg" />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/helper/ExploreProducts.js
+++ src/helper/ExploreProducts.js
@@ -7,10 +7,15 @@ export const Images = [
       "https://m.media-amazon.com/images/I/41ObOT7XPNL.jpg",
       "https://images.jdmagicbox.com/quickquotes/images_main/asus-zenbook-14-ultra-thin-and-light-laptop-4-way-nanoedge-14-full-hd-intel-core-i7-8565u-8gb-lpddr3-ram-512gb-nvme-pcie-ssd-wi-fi-5-windows-10-silver-blue-ux431fa-es74-179008469-q9czu.jpg",
     "https://img.freepik.com/premium-psd/white-headphone-mockup_317810-3959.jpg?size=626&ext=jpg&ga=GA1.1.1413502914.1696464000&semt=ais",
-    "https://media.istockphoto.com/id/993760082/photo/3d-set-of-home-appliances-on-white-background.jpg?s=170667a&w=0&k=20&c=F9f78ok7E4YYn0Cz9APQ3z_wQ__HRCtyXwyrR4CdqBE="
+    "https://rukminim2.flixcart.com/image/416/416/xif0q/mobile/1/t/i/-original-imagrn6ds5mnccnt.jpeg?q=70"
     ],
     title:"Redmi note 12 pro plus",
-    description:"nedhwdbjhwexwewdkwjdewbejhs"
+    description:["8 GB RAM | 256 GB ROM",
+     " 17.02 cm (6.7 inch) Full HD+ Display",
+      "64MP + 32MP + 8MP | 32MP Front Camera",
+      "5000 mAh Battery",
+      "Dimensity 7050 Processor"
+  ]
   },
   {
     id: 2,
@@ -23,7 +28,11 @@ export const Images = [
     "https://media.istockphoto.com/id/993760082/photo/3d-set-of-home-appliances-on-white-background.jpg?s=170667a&w=0&k=20&c=F9f78ok7E4YYn0Cz9APQ3z_wQ__HRCtyXwyrR4CdqBE="
     ],
     title:"Vivo y100 ",
-    description:"nedhwdbjhwexwewdkwjdewbejhs"
+    description:["8 GB RAM | 256 GB ROM",
+     " 17.02 cm (6.7 inch) Full HD+ Display",
+      "64MP + 32MP + 8MP | 32MP Front Camera",
+      "5000 mAh Battery",
+      "Dimensity 7050 Processor"]
   },
  
 ];
